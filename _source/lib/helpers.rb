2a1517244ddc7b9750c15304a58b143d65031b09
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-11-22 22:07:35 +0100

    Adding yard-style links to broker guide. Will test & see if they work fine here. If so, will with the rest of the website to use those, too.

--- _source/lib/helpers.rb
+++ _source/lib/helpers.rb
@@ -1,11 +1,12 @@
 require 'liquid'
 
 module Helpers
-  # Render gist by given :id
+  #
+  # Render file & gist example
   #
   # Usage:
   #
-  # {% gist id %} =>
+  # {% example "file_to_expand", gist_id %} =>
   #
   # Raises: Liquid::SyntaxError
   class Example < ::Liquid::Tag
@@ -29,9 +30,52 @@ module Helpers
       %{<pre>#{File.open("./content/articles/examples/#{@name}", "rb").read}</pre><span class="help-block">(if the example above isn't displayed, see this "gist":https://gist.github.com/#{@gist_id})</span>}
     end
   end
+
+  class YardLink < ::Liquid::Tag
+
+    Syntax = /(#{::Liquid::Expression}+)?/
+
+    def initialize(tag_name, markup, tokens)
+      if markup =~ Syntax
+        @class_name = $1
+        @options = {}
+        markup.scan(::Liquid::TagAttributes) { |key, value| @options[key.to_sym] = value.gsub(/"|'/, '') }
+      else
+        raise ::Liquid::SyntaxError.new("Syntax Error in 'link' - Valid syntax: gist <id> <options>")
+      end
+
+      super
+    end
+
+    def calculate_path full_name
+      path = full_name.gsub("::", "/").gsub(".", "#")
+      unless is_class?(full_name)
+        if is_instance_method?(full_name)
+          path += "-instance_method"
+        else
+          path += "-class_method"
+        end
+      end
+      path
+    end
+
+    def is_instance_method? full_name
+      !(full_name =~ /\#/).nil?
+    end
+
+    def is_class? full_name
+      (full_name =~ /\./ || full_name =~ /\#/).nil?
+    end
+
+    def render(context)
+      %{<a class="highlight" href="http://rubydoc.info/github/ruby-amqp/amqp/master/#{calculate_path(@class_name)}">#{@class_name}</a>}
+    end
+  end
+
 end
 
 Liquid::Template.register_tag('example', Helpers::Example)
+Liquid::Template.register_tag('yard_link', Helpers::YardLink)
 
 class LiquidFilter < Nanoc3::Filter
   identifier :liquid
